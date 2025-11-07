7a69e5350a798a020e80c6d0eca7e5eafa56a2be
ProjectCaffeine <smith_eg21@yahoo.com>
2025-11-06 19:39:04 -0600

    Hello World

---
+++ nexus.merge/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var ints = new List<int>
+{
+    1,
+    2,
+    3,
+    4
+};
+
+var first = ints.FirstOrDefault(3);
+
+Console.WriteLine(first);
