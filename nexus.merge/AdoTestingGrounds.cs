e763242db53c1815ade65b7c9fe8fb369d9e1df4
ProjectCaffeine <smith_eg21@yahoo.com>
2025-11-12 21:22:04 -0600

    resolved conflicts

--- nexus.merge/AdoTestingGrounds.cs
+++ nexus.merge/AdoTestingGrounds.cs
@@ -2,31 +2,30 @@ using Microsoft.TeamFoundation.SourceControl.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
 
-namespace nexus.merge
+namespace nexus.merge;
+
+public static class AdoTestingGrounds
 {
-  public static class AdoTestingGrounds
+  public static async Task GetBranchesAsync()
   {
-    public static async Task GetBranchesAsync()
+    Console.WriteLine("Connecting.");
+
+    VssConnection connection = new(new Uri("https://dev.azure.com/ProjectCaffeine"), new VssBasicCredential(string.Empty, ""));
+
+    Console.WriteLine("Getting Client");
+    var project = "NexusMergeProvingGrounds";
+    var repositoryId = "NexusMergeProvingGrounds";
+
+    GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+    GitRepository repository = await gitClient.GetRepositoryAsync(project, repositoryId);
+
+    Console.WriteLine($"default branch: {repository.DefaultBranch}");
+
+    var branches = await gitClient.GetBranchesAsync(project, repositoryId);
+
+    foreach (var branch in branches)
     {
-      Console.WriteLine("Connecting.");
-
-      VssConnection connection = new(new Uri("https://dev.azure.com/ProjectCaffeine"), new VssBasicCredential(string.Empty, ""));
-
-      Console.WriteLine("Getting Client");
-      var project = "NexusMergeProvingGrounds";
-      var repositoryId = "NexusMergeProvingGrounds";
-
-      GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
-      GitRepository repository = await gitClient.GetRepositoryAsync(project, repositoryId);
-
-      Console.WriteLine($"default branch: {repository.DefaultBranch}");
-
-      var branches = await gitClient.GetBranchesAsync(project, repositoryId);
-
-      foreach (var branch in branches)
-      {
-        Console.WriteLine(branch.Name);
-      }
+      Console.WriteLine(branch.Name);
     }
   }
 }
