8276ab92e96ed48b06e7bc869f2017f0921f6c3f
ProjectCaffeine <smith_eg21@yahoo.com>
2025-11-12 21:13:11 -0600

    Began adding ability to talk to git

---
+++ nexus.merge/AdoTestingGrounds.cs
@@ -0,0 +1,32 @@
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace nexus.merge
+{
+  public static class AdoTestingGrounds
+  {
+    public static async Task GetBranchesAsync()
+    {
+      Console.WriteLine("Connecting.");
+
+      VssConnection connection = new(new Uri("https://dev.azure.com/ProjectCaffeine"), new VssBasicCredential(string.Empty, ""));
+
+      Console.WriteLine("Getting Client");
+      var project = "NexusMergeProvingGrounds";
+      var repositoryId = "NexusMergeProvingGrounds";
+
+      GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+      GitRepository repository = await gitClient.GetRepositoryAsync(project, repositoryId);
+
+      Console.WriteLine($"default branch: {repository.DefaultBranch}");
+
+      var branches = await gitClient.GetBranchesAsync(project, repositoryId);
+
+      foreach (var branch in branches)
+      {
+        Console.WriteLine(branch.Name);
+      }
+    }
+  }
+}
--- nexus.merge/Program.cs
+++ nexus.merge/Program.cs
@@ -1,19 +1,28 @@
-﻿//using Terminal.Gui;
-//
-//Application.Init();
-//
-//try
-//{
-//  Application.Run<MainWindow>();
-//}
-//catch (Exception ex)
-//{
-//  var test = "test";
-//}
-//finally
-//{
-//  Application.Shutdown();
-//}
+﻿
+using System.Diagnostics;
 
+string command = "status";
 
+using (Process process = new())
+{
+  process.StartInfo.WorkingDirectory = "/home/omfarm/Repos/NexusMergeProvingGrounds";
+  process.StartInfo.FileName = "git";
+  process.StartInfo.Arguments = command;
+  process.StartInfo.UseShellExecute = false;
+  process.StartInfo.RedirectStandardError = true;
+  process.StartInfo.RedirectStandardOutput = true;
+  process.StartInfo.CreateNoWindow = true;
 
+  process.Start();
+
+  string output = await process.StandardOutput.ReadToEndAsync();
+  string error = await process.StandardError.ReadToEndAsync();
+
+  process.WaitForExit();
+
+  Console.WriteLine("--- Command Output ---");
+  Console.WriteLine(output);
+  Console.WriteLine("--- Command Error ---");
+  Console.WriteLine(error);
+  Console.WriteLine($"Exit code: {process.ExitCode}");
+}
